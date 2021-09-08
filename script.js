3d791caa647771e1a81cab94e95f87e87b77a69c
deviator53 <davidbradforduko@yahoo.com>
2021-09-08 17:58:12 +0100

    Added Javascript file

---
+++ script.js
@@ -0,0 +1,8 @@
+const show = document.querySelector('#show');
+// console.log(show);
+const sidebar = document.querySelector('.sidebar');
+
+show.addEventListener('click', function(){
+    sidebar.classList.toggle('guy-show');
+});
+console.log(3);
